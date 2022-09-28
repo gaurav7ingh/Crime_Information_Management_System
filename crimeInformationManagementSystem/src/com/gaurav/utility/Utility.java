1dd3c5aa7d10fbb48770103e479de2fe155d247e
gaurav7ingh <gauravsingh99626@gmail.com>
2022-09-28 22:15:30 +0530

    DONE : utility class is created and also the table structure is created

---
+++ crimeInformationManagementSystem/src/com/gaurav/utility/Utility.java
@@ -0,0 +1,29 @@
+package com.gaurav.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Utility {
+	
+	public static Connection getConnection() {
+		Connection conn = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		String url = "jdbc:mysql://localhost:3306/crimedb";
+		
+		try {
+			conn = DriverManager.getConnection(url,"root", "root");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return conn;
+	}
+}
